5b78ebbe0a9729efd23ec85e92707a79054689f8
Butterfly Of My Mind <66660068+arkajyotiadhikary@users.noreply.github.com>
2021-02-06 23:50:35 +0530

    tutorial 1

---
+++ Proj1/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offsets = new Vector3(0,5,-7);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offsets;
+    }
+}
